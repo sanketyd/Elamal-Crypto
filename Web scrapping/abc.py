e95737c075504e3b7481e1ebcdfea83997ff7e33
sanketyd <yadavsanket17@yahoo.in>
2016-06-15 01:49:52 +0530

    final

--- Web scrapping/abc.py
+++ Web scrapping/abc.py
@@ -2,15 +2,13 @@ import urllib2
 from bs4 import BeautifulSoup
 import sqlite3
 conn=sqlite3.connect('students.db')
-#import subprocess
 c=conn.cursor()
 #c.execute('''CREATE TABLE students
 	#(name text,roll_no text,imgsrc text)''')
 
-f= open('a.txt','w')
-for i in range(150001,150846):
+for i in range(150100,150846):
 	url="http://home.iitk.ac.in/~romit/studentsearch/profile.php?view="+str(i)
-	page=urllib2.urlopen(url).read()#subprocess.call(['curl',url])
+	page=urllib2.urlopen(url).read()
 	soup=BeautifulSoup(page,'html.parser')
 	div1=soup.find_all("div",{"id":"values"})
 	img=soup.find_all("img")
@@ -22,5 +20,4 @@ for i in range(150001,150846):
 		
 	x=div2.split('\n')
 	c.execute("INSERT INTO students VALUES (?,?,?)",(str(x[1]),str(x[2]),str(imgsrc)))
-	#f.write(x[1]+'\t'+x[2]+'\t'+imgsrc)
 	conn.commit()
